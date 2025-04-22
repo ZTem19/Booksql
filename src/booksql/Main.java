93f43e00ca6b03787f6f3e3a99ef7c47fd0bacbe
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 <zander.templin@gmail.com>
2025-04-22 14:41:33 -0500

    Setup refactor

--- src/booksql/Main.java
+++ src/booksql/Main.java
@@ -4,6 +4,8 @@
  */
 package booksql;
 
+import java.sql.*;
+
 /**
  *
  * @author zander
@@ -15,6 +17,21 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("Hello world! we love books books books.");
+        
+        String jdbcUrl = "jdbc:postgresql://localhost:5432/inclass_db";
+        String username = "";
+        String password = "";
+        
+        Connection connection;
+        
+        try {
+            Class.forName("org.postgresql.Driver");
+            System.out.println("Loaded Driver");
+            connection = DriverManager.getConnection(jdbcUrl, username, password);
+            System.out.println("Made connection");
+        } catch(Exception e){
+            System.out.println("Issue connecting to the postgresql db.");
+        }
         // TODO code application logic here
     }
     
